07dc956b10684d839d7231dfe3f7dff8db723a17
vladyslavnUA <vladyslavn@yahoo.com>
2019-12-12 01:49:46 -0800

    updated files

--- app.py
+++ app.py
@@ -3,6 +3,75 @@ from pymongo import MongoClient
 from bson.objectid import ObjectId
 import os
 
+<<<<<<< HEAD
+
+host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/sunglasses')
+client = MongoClient(host=host)
+db = client.get_default_database()
+sunglasses = db.sunglasses
+
+
+app = Flask(__name__)
+
+# @app.route("/login")
+# def login:
+# 	auth = request.authorization
+# 	return " "
+
+@app.route("/")
+def sunglasses_index():
+	#this will show the type of pants we have
+	return render_template("sunglasses_index.html", sunglasses=sunglasses.find())
+
+@app.route("/sunglasses", methods=["POST"])
+def sunglasses_submit():
+	sunglass = {
+		"sunglass_name": request.form.get("sunglass_name"),
+		"description": request.form.get("description"),
+		"price": request.form.get("price"),
+        "brand": request.form.get("brand")
+	}
+	sunglass_id = sunglasses.insert_one(sunglass).inserted_id
+	print(sunglass_id)
+	return redirect(url_for("sunglasses_show", sunglass_id = sunglass_id))
+
+@app.route("/sunglasses/<sunglass_id>")
+def sunglasses_show(sunglass_id):
+	sunglass = sunglass.find_one({'_id' : ObjectId(sunglass_id)})
+	return render_template("sunglasses_show.html", sunglass = sunglass)
+
+@app.route("/sunglasses/new")
+def sunglasses_new():
+	return render_template("pants_new.html", pant ={}, title ="New pant")
+
+@app.route("/pants/<pant_id>/edit")
+def pants_edit(pant_id):
+	pant = pants.find_one({"_id" : ObjectId(pant_id)})
+	return render_template("pants_edit.html", pant = pant, title = "Edit pant")
+
+@app.route("/pants/<pant_id>", methods = ['POST'])
+def pants_update(pant_id):
+	updated_pant = {
+		"pants_name": request.form.get("pants_name"),
+		"description": request.form.get("description"),
+		"price": request.form.get("price"),
+        "color": request.form.get("color")
+
+	}
+
+	pants.update_one( {"_id" : ObjectId(pant_id)}, {"$set" : updated_pant})
+	return redirect(url_for("pants_show", pant_id = pant_id))
+
+
+@app.route("/pants/<pant_id>/delete", methods=["POST"])
+def pants_delete(pant_id):
+	pants.delete_one({"_id" : ObjectId(pant_id)})
+	return redirect(url_for("pants_index"))
+
+
+if __name__ == "__main__":
+	app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
+=======
 host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Wearoid')
 client = MongoClient(host=f'{host}?retryWrites=false') #mongo client
 app = Flask(__name__)
@@ -88,3 +157,4 @@ def comments_delete(comment_id):
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
+>>>>>>> 6ac627d1e88230be791d63698568f952ae265ae0
